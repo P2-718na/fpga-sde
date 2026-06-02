94108cfd747788fbdf09070d6b93580e0857747a
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2026-06-02 12:46:38 +0200

    New version with packing that still runs at 15Ms/s

--- sde-accel/sde-accel.cpp
+++ sde-accel/sde-accel.cpp
@@ -1,33 +1,66 @@
 #include "sde-accel.hpp"
 
+static void unpack_job(
+    JobPacked packed,
+    fixed_t in[NUM_INPUTS]
+)
+{
+#pragma HLS INLINE
+
+    for(int i=0;i<NUM_INPUTS;i++) {
+#pragma HLS UNROLL
+
+        ap_int<32> tmp =
+            packed.range(32*i+31,32*i);
+
+        in[i].range() = tmp;
+    }
+}
+
+static ResultPacked pack_result(
+    fixed_t out[NUM_OUTPUTS]
+)
+{
+#pragma HLS INLINE
+
+    ResultPacked packed = 0;
+
+    for(int i=0;i<NUM_OUTPUTS;i++) {
+#pragma HLS UNROLL
+
+        packed.range(32*i+31,32*i) =
+            out[i].range();
+    }
+
+    return packed;
+}
+
 static void compute_job(
     fixed_t in[NUM_INPUTS],
     fixed_t out[NUM_OUTPUTS]
-) {
-
+)
+{
 #pragma HLS INLINE
 
     fixed_t sum = 0;
 
-    // Parallel input accumulation
-    for (int i = 0; i < NUM_INPUTS; i++) {
+    for(int i=0;i<NUM_INPUTS;i++) {
 #pragma HLS UNROLL
         sum += in[i];
     }
 
-    // Parallel output generation
-    for (int o = 0; o < NUM_OUTPUTS; o++) {
+    for(int i=0;i<NUM_OUTPUTS;i++) {
 #pragma HLS UNROLL
-        out[o] = sum + o;
+        out[i] = sum + i;
     }
 }
 
 void accelerator(
-    fixed_t jobs[][NUM_INPUTS],
-    fixed_t results[][NUM_OUTPUTS],
+    JobPacked* jobs,
+    ResultPacked* results,
     int num_jobs
-) {
-
+)
+{
 #pragma HLS INTERFACE m_axi port=jobs    offset=slave bundle=gmem
 #pragma HLS INTERFACE m_axi port=results offset=slave bundle=gmem
 
@@ -37,32 +70,22 @@ void accelerator(
 #pragma HLS INTERFACE s_axilite port=return
 
 MAIN_LOOP:
-    for (int i = 0; i < num_jobs; i++) {
+    for(int i=0;i<num_jobs;i++) {
 
 #pragma HLS PIPELINE II=1
 
-#pragma HLS DEPENDENCE variable=jobs inter false
-#pragma HLS DEPENDENCE variable=results inter false
+        fixed_t in[NUM_INPUTS];
+        fixed_t out[NUM_OUTPUTS];
 
-        fixed_t in_local[NUM_INPUTS];
-        fixed_t out_local[NUM_OUTPUTS];
+#pragma HLS ARRAY_PARTITION variable=in complete
+#pragma HLS ARRAY_PARTITION variable=out complete
 
-#pragma HLS ARRAY_PARTITION variable=in_local complete
-#pragma HLS ARRAY_PARTITION variable=out_local complete
+        JobPacked j = jobs[i];
 
-        // Read inputs
-        for (int k = 0; k < NUM_INPUTS; k++) {
-#pragma HLS UNROLL
-            in_local[k] = jobs[i][k];
-        }
+        unpack_job(j, in);
 
-        // Compute
-        compute_job(in_local, out_local);
+        compute_job(in, out);
 
-        // Write outputs
-        for (int k = 0; k < NUM_OUTPUTS; k++) {
-#pragma HLS UNROLL
-            results[i][k] = out_local[k];
-        }
+        results[i] = pack_result(out);
     }
 }
--- sde-accel/sde-accel.hpp
+++ sde-accel/sde-accel.hpp
@@ -2,15 +2,19 @@
 #define ACCELERATOR_HPP
 
 #include <ap_fixed.h>
+#include <ap_int.h>
 
-#define NUM_INPUTS   6
-#define NUM_OUTPUTS  4
+#define NUM_INPUTS  6
+#define NUM_OUTPUTS 4
 
-typedef ap_fixed<32, 8> fixed_t;
+typedef ap_fixed<32,8> fixed_t;
+
+typedef ap_uint<256> JobPacked;
+typedef ap_uint<128> ResultPacked;
 
 void accelerator(
-    fixed_t jobs[][NUM_INPUTS],
-    fixed_t results[][NUM_OUTPUTS],
+    JobPacked* jobs,
+    ResultPacked* results,
     int num_jobs
 );
 
