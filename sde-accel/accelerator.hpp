54cd561929ae20648ca9903d30da384d58a8a846
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2026-06-16 16:31:10 +0200

    Version with Euler+noise integrator. Noise part is Already tested and it works

--- sde-accel/accelerator.hpp
+++ sde-accel/accelerator.hpp
@@ -4,16 +4,33 @@
 #include <ap_fixed.h>
 #include <ap_int.h>
 
-// Inputs are u, du, v, dv and I for each neuron (job)
-// Outputs are just u, du, v, dv
-#define NUM_INPUTS  5
-#define NUM_OUTPUTS 4
+// FIXME All of this defines should probably be typedefs.
+// They still allow for unroll
+
+// Inputs are u, v and I for each neuron (job)
+// Outputs are just u, v.
+// Since FH is first-order, we don't need the derivatives...
+#define NUM_INPUTS  3
+#define NUM_OUTPUTS 2
 
 typedef ap_fixed<32,8> fixed_t;
 
+// How many LCG to use for gaussian generator
+#define GAUSS_UNIFORMS 8
+
+#if GAUSS_UNIFORMS == 4
+#define CLT_SCALE (fixed_t)1.73205080757;
+#elif GAUSS_UNIFORMS == 8
+#define CLT_SCALE (fixed_t)1.22474487139;
+#elif GAUSS_UNIFORMS == 12
+#define CLT_SCALE (fixed_t)1.0;
+#else
+    #error "Unsupported GAUSS_UNIFORMS"
+#endif
+
 // HP ports have bandwidth of 64; no need to pad to 256
-typedef ap_uint<192> JobPacked;
-typedef ap_uint<128> ResultPacked;
+typedef ap_uint<128> JobPacked;
+typedef ap_uint<64> ResultPacked;
 
 void accelerator(
     JobPacked* jobs,
@@ -21,7 +38,10 @@ void accelerator(
     int num_jobs,
     fixed_t dt,
     fixed_t a,
-    fixed_t e
+    fixed_t inv_e,
+    fixed_t sigma_sqrt_dt,
+    ap_uint<32> seed,
+    bool reseed
 );
 
 #endif
--- sde-accel/sde-accel.cpp
+++ sde-accel/sde-accel.cpp
@@ -4,11 +4,11 @@ static void unpack_job(JobPacked packed, fixed_t in[NUM_INPUTS]) {
 // We inline since this is essentially just an abstraction layer    
 #pragma HLS INLINE
 
-    for(int i=0;i<NUM_INPUTS;i++) {
+    for(int i = 0; i < NUM_INPUTS; ++i) {
     #pragma HLS UNROLL
 
         ap_int<32> tmp =
-            packed.range(32*i+31,32*i);
+            packed.range(32*i + 31, 32*i);
 
         in[i].range() = tmp;
     }
@@ -20,14 +20,55 @@ static ResultPacked pack_result(fixed_t out[NUM_OUTPUTS]) {
 
     ResultPacked packed = 0;
 
-    for(int i=0;i<NUM_OUTPUTS;i++) {
+    for(int i = 0; i < NUM_OUTPUTS; ++i) {
     #pragma HLS UNROLL
 
-        packed.range(32*i+31,32*i) =
-            out[i].range();
+        packed.range(32*i + 31, 32*i) = out[i].range();
     }
 
     return packed;
+}
+
+// Gotta use ap_uint so that we can call the .range function
+static ap_uint<32> xorshift32(ap_uint<32>& state) {
+#pragma HLS INLINE
+
+    state ^= state << 13;
+    state ^= state >> 17;
+    state ^= state << 5;
+
+    return state;
+}
+
+// Uniform generator between 0 and
+static fixed_t uniform01(ap_uint<32>& state)
+{
+#pragma HLS INLINE
+
+    // Here, we want to generate a random uniform between 0 and 1.
+    // Since we are using the q8.24 format, one way to do so is just discard the 8 integer bits
+    // and just use the upper 24 bits of the generator as the fractional bits.
+    // We loose precision, but now the distribution properties (mean and variane) are known and small (same order of 1). 
+    ap_uint<32> r = xorshift32(state);
+    ap_ufixed<24,0> frac;
+    // Range just copies elements bitwise
+    frac.range(23,0) = r.range(31,8);
+
+    return (fixed_t)frac;
+}
+
+static fixed_t gaussian_clt(ap_uint<32> rng_state[GAUSS_UNIFORMS]) {
+#pragma HLS INLINE
+
+    fixed_t sum = 0;
+
+    for(int i = 0; i < GAUSS_UNIFORMS; ++i) {
+    #pragma HLS UNROLL
+        sum += uniform01(rng_state[i]);
+    }
+
+    // TODO maybe properly check gen scales if they make sense
+    return (sum - (fixed_t)(GAUSS_UNIFORMS/2)) * CLT_SCALE;
 }
 
 static void compute_job(
@@ -35,26 +76,23 @@ static void compute_job(
     fixed_t out[NUM_OUTPUTS],
     fixed_t dt,
     fixed_t a,
-    fixed_t inv_e
-) {
-// This might have to be removed when genning rng numbers
+    fixed_t inv_e,
+    fixed_t sigma_sqrt_dt,
+    ap_uint<32> rng_state[GAUSS_UNIFORMS]
+)
+{
 #pragma HLS INLINE
 
-    const fixed_t u     = in[0];
-    const fixed_t u_dot = in[1];
-    const fixed_t v     = in[2];
-    const fixed_t v_dot = in[3];
-    const fixed_t I     = in[4];
+    const fixed_t u = in[0];
+    const fixed_t v = in[1];
+    const fixed_t I = in[2];
 
-    const fixed_t u_new = u + u_dot * dt;
-    const fixed_t u_dot_new = (u - u*u*u/(fixed_t)3 - v + I) * inv_e; // TODO I believe the /3 gets compiled out im too bothered to check it myself
-    const fixed_t v_new = v + v_dot * dt;
-    const fixed_t v_dot_new = u + a;
+    const fixed_t du = (u - u*u*u/(fixed_t)3 - v + I) * inv_e;
+    const fixed_t dv = u + a;
+    const fixed_t noise = sigma_sqrt_dt * gaussian_clt(rng_state);
 
-    out[0] = u_new;
-    out[1] = u_dot_new;
-    out[2] = v_new;
-    out[3] = v_dot_new;
+    out[0] = u + du * dt + noise;
+    out[1] = v + dv * dt;
 }
 
 void accelerator(
@@ -63,7 +101,10 @@ void accelerator(
     int num_jobs,
     fixed_t dt,
     fixed_t a,
-    fixed_t inv_e
+    fixed_t inv_e,
+    fixed_t sigma_sqrt_dt,
+    ap_uint<32> seed,
+    bool reseed
 ) {
     // TODO CHECK BURST SIZE
 #pragma HLS INTERFACE m_axi port=jobs    offset=slave bundle=gmem0
@@ -76,8 +117,38 @@ void accelerator(
 #pragma HLS INTERFACE s_axilite port=dt
 #pragma HLS INTERFACE s_axilite port=a
 #pragma HLS INTERFACE s_axilite port=inv_e
+#pragma HLS INTERFACE s_axilite port=sigma_sqrt_dt
+#pragma HLS INTERFACE s_axilite port=seed
+#pragma HLS INTERFACE s_axilite port=reseed
 
 #pragma HLS INTERFACE s_axilite port=return
+
+
+SETUP:
+    // reseed is actually optional, we could just say "set seed to X when you don't wanna reseed"
+    // But it is clearer this way
+    static ap_uint<32> rng_state[GAUSS_UNIFORMS] = {
+        0x12345678,
+        0x9E3779B9,
+        0x243F6A88,
+        0xB7E15162,
+        0xDEADBEEF,
+        0xCAFEBABE,
+        0x8BADF00D,
+        0xFEEDFACE
+    };
+
+    #pragma HLS ARRAY_PARTITION variable=rng_state complete
+    if(reseed) {
+        rng_state[0] = seed;
+        rng_state[1] = seed ^ 0x9E3779B9;
+        rng_state[2] = seed ^ 0x243F6A88;
+        rng_state[3] = seed ^ 0xB7E15162;
+        rng_state[4] = seed ^ 0xDEADBEEF;
+        rng_state[5] = seed ^ 0xCAFEBABE;
+        rng_state[6] = seed ^ 0x8BADF00D;
+        rng_state[7] = seed ^ 0xFEEDFACE;
+    }
 
 MAIN_LOOP:
     for(int i=0; i < num_jobs; ++i) {
@@ -93,7 +164,7 @@ MAIN_LOOP:
 
         const JobPacked j = jobs[i];
         unpack_job(j, in);
-        compute_job(in, out, dt, a, inv_e);
+        compute_job(in, out, dt, a, inv_e, sigma_sqrt_dt, rng_state);
         results[i] = pack_result(out);
     }
 }
