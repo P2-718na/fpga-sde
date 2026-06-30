3e971a3af4511c5e93b70b0c203350fecb6535ad
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2026-06-30 17:41:35 +0200

    Single buffering mode

--- sde-accel/accelerator.hpp
+++ sde-accel/accelerator.hpp
@@ -4,37 +4,32 @@
 #include <ap_fixed.h>
 #include <ap_int.h>
 
-// FIXME All of this defines should probably be typedefs.
-// They still allow for unroll
-
-// Inputs are u, v and I for each neuron (job)
-// Outputs are just u, v.
-// Since FH is first-order, we don't need the derivatives...
-#define NUM_INPUTS  3
-#define NUM_OUTPUTS 2
-
 typedef ap_fixed<32,8> fixed_t;
 
-// How many LCG to use for gaussian generator
 #define GAUSS_UNIFORMS 8
 
 #if GAUSS_UNIFORMS == 4
-#define CLT_SCALE (fixed_t)1.73205080757;
+#define CLT_SCALE (fixed_t)1.73205080757
 #elif GAUSS_UNIFORMS == 8
-#define CLT_SCALE (fixed_t)1.22474487139;
+#define CLT_SCALE (fixed_t)1.22474487139
 #elif GAUSS_UNIFORMS == 12
-#define CLT_SCALE (fixed_t)1.0;
+#define CLT_SCALE (fixed_t)1.0
 #else
-    #error "Unsupported GAUSS_UNIFORMS"
+#error "Unsupported GAUSS_UNIFORMS"
 #endif
 
-// HP ports have bandwidth of 64; no need to pad to 256
-typedef ap_uint<128> JobPacked;
-typedef ap_uint<64> ResultPacked;
+struct State {
+    fixed_t u;
+    fixed_t v;
+};
+
+// Make sure we are correcyly packing to 64 bit words.
+// Writing it this way avoids the ugly pack-unpack functions
+static_assert(sizeof(State) == 8);
 
 void accelerator(
-    JobPacked* jobs,
-    ResultPacked* results,
+    State* state,
+    fixed_t* I,
     int num_jobs,
     fixed_t dt,
     fixed_t a,
--- sde-accel/sde-accel.cpp
+++ sde-accel/sde-accel.cpp
@@ -1,36 +1,5 @@
 #include "accelerator.hpp"
 
-static void unpack_job(JobPacked packed, fixed_t in[NUM_INPUTS]) {
-UNPACK:
-// We inline since this is essentially just an abstraction layer    
-#pragma HLS INLINE
-
-    for(int i = 0; i < NUM_INPUTS; ++i) {
-    #pragma HLS UNROLL
-
-        ap_int<32> tmp =
-            packed.range(32*i + 31, 32*i);
-
-        in[i].range() = tmp;
-    }
-}
-
-static ResultPacked pack_result(fixed_t out[NUM_OUTPUTS]) {
-PACK:
-// Again, inline since this is just an abstraction.
-#pragma HLS INLINE
-
-    ResultPacked packed = 0;
-
-    for(int i = 0; i < NUM_OUTPUTS; ++i) {
-    #pragma HLS UNROLL
-
-        packed.range(32*i + 31, 32*i) = out[i].range();
-    }
-
-    return packed;
-}
-
 // Gotta use ap_uint so that we can call the .range function
 static ap_uint<32> xorshift32(ap_uint<32>& state) {
 #pragma HLS INLINE
@@ -74,8 +43,9 @@ static fixed_t gaussian_clt(ap_uint<32> rng_state[GAUSS_UNIFORMS]) {
 }
 
 static void compute_job(
-    fixed_t in[NUM_INPUTS],
-    fixed_t out[NUM_OUTPUTS],
+    const State& in,
+    fixed_t I,
+    State& out,
     fixed_t dt,
     fixed_t a,
     fixed_t inv_e,
@@ -83,25 +53,24 @@ static void compute_job(
     ap_uint<32> rng_state[GAUSS_UNIFORMS]
 )
 {
-COMPUTE:
 #pragma HLS INLINE
-    const fixed_t u = in[0];
-    const fixed_t v = in[1];
-    const fixed_t I = in[2];
 
-    const fixed_t du = (u - u*u*u*(fixed_t)(1/3) - v + I) * inv_e;
+    const fixed_t u = in.u;
+    const fixed_t v = in.v;
+
+    const fixed_t du = (u - u*u*u*(fixed_t)(1.0/3.0) - v + I) * inv_e;
     const fixed_t dv = u + a;
     // Qui non sto dividendo per epsilon per salvare risorse.
     // La temperatura lato python deve contenere il fattore giusto.
     const fixed_t noise = sigma_sqrt_dt * gaussian_clt(rng_state);
-
-    out[0] = u + du * dt + noise;
-    out[1] = v + dv * dt;
+    
+    out.u = u + du * dt + noise;
+    out.v = v + dv * dt;
 }
 
 void accelerator(
-    JobPacked* jobs,
-    ResultPacked* results,
+    State* state,
+    fixed_t* I,
     int num_jobs,
     fixed_t dt,
     fixed_t a,
@@ -109,13 +78,16 @@ void accelerator(
     fixed_t sigma_sqrt_dt,
     ap_uint<32> seed,
     bool reseed
-) {
-    // TODO CHECK BURST SIZE
-#pragma HLS INTERFACE m_axi port=jobs    offset=slave bundle=gmem0
-#pragma HLS INTERFACE m_axi port=results offset=slave bundle=gmem1
+)
+{
+// Make sure the packing is happening for state
+#pragma HLS aggregate variable=state
+    
+#pragma HLS INTERFACE m_axi port=state offset=slave bundle=gmem0 // num_read_outstanding=16 num_write_outstanding=16 didn't help with II2
+#pragma HLS INTERFACE m_axi port=I     offset=slave bundle=gmem1
 
-#pragma HLS INTERFACE s_axilite port=jobs
-#pragma HLS INTERFACE s_axilite port=results
+#pragma HLS INTERFACE s_axilite port=state 
+#pragma HLS INTERFACE s_axilite port=I
 #pragma HLS INTERFACE s_axilite port=num_jobs
 
 #pragma HLS INTERFACE s_axilite port=dt
@@ -124,13 +96,8 @@ void accelerator(
 #pragma HLS INTERFACE s_axilite port=sigma_sqrt_dt
 #pragma HLS INTERFACE s_axilite port=seed
 #pragma HLS INTERFACE s_axilite port=reseed
-
 #pragma HLS INTERFACE s_axilite port=return
 
-
-SETUP:
-    // reseed is actually optional, we could just say "set seed to X when you don't wanna reseed"
-    // But it is clearer this way
     static ap_uint<32> rng_state[GAUSS_UNIFORMS] = {
         0x12345678,
         0x9E3779B9,
@@ -142,8 +109,10 @@ SETUP:
         0xFEEDFACE
     };
 
-    #pragma HLS ARRAY_PARTITION variable=rng_state complete
-    if(reseed) {
+#pragma HLS ARRAY_PARTITION variable=rng_state complete
+
+    if(reseed)
+    {
         rng_state[0] = seed;
         rng_state[1] = seed ^ 0x9E3779B9;
         rng_state[2] = seed ^ 0x243F6A88;
@@ -154,21 +123,32 @@ SETUP:
         rng_state[7] = seed ^ 0xFEEDFACE;
     }
 
+// Tell vitis that processing for all inputs can be done independently
+#pragma HLS DEPENDENCE variable=state inter false
+
 MAIN_LOOP:
-    for(int i=0; i < num_jobs; ++i) {
-    // Thanks compiler
-    #pragma HLS PIPELINE II=1
+    for(int i = 0; i < num_jobs; ++i)
+    {
+#pragma HLS PIPELINE II=1
 
-        fixed_t in[NUM_INPUTS];
-        fixed_t out[NUM_OUTPUTS];
+        State current;
+        State next;
 
-        // Access all array elements simultaneously
-        #pragma HLS ARRAY_PARTITION variable=in complete
-        #pragma HLS ARRAY_PARTITION variable=out complete
+#pragma HLS ARRAY_PARTITION variable=current complete
+#pragma HLS ARRAY_PARTITION variable=next complete
 
-        const JobPacked j = jobs[i];
-        unpack_job(j, in);
-        compute_job(in, out, dt, a, inv_e, sigma_sqrt_dt, rng_state);
-        results[i] = pack_result(out);
+        current = state[i];
+
+        compute_job(
+            current,
+            I[i],
+            next,
+            dt,
+            a,
+            inv_e,
+            sigma_sqrt_dt,
+            rng_state);
+
+        state[i] = next;
     }
 }
