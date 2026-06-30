9c67549020f271a53b90f4c1e6645ee496f6c44d
Matteo Bonacini <26163503+P2-718na@users.noreply.github.com>
2026-06-30 19:43:30 +0200

    Remove crap

--- network-accel/build/hls/.autopilot/db/coregen/control.slave.tcl
+++
@@ -1,4 +0,0 @@
-dict set slaves control {ports {row_ptr_ddr {type i_ap_none width 64} col_idx_ddr {type i_ap_none width 64} u_ddr {type i_ap_none width 64} v_ddr {type i_ap_none width 64} neuron_count {type i_ap_none width 32} edge_count {type i_ap_none width 32} timesteps {type i_ap_none width 32} command {type i_ap_none width 32} dt {type i_ap_none width 32} a {type i_ap_none width 32} J {type i_ap_none width 32} inv_e {type i_ap_none width 32} sigma_sqrt_dt {type i_ap_none width 32} seed {type i_ap_none width 32} reseed {type i_ap_none width 1} ap_start {type ap_ctrl width 1} ap_done {type ap_ctrl width 1} ap_ready {type ap_ctrl width 1} ap_idle {type ap_ctrl width 1}} mems {} has_ctrl 1}
-set datawidth 32
-set addrwidth 64
-set intr_clr_mode TOW
